43e2e74dbf3acf5b08cbd3f0c574cbf95bf2f8bf
陈浩明 <christian.chan@foxmail.com>
2019-05-05 18:19:37 +0800

    Added unitest for creating an empty folder with project name

--- src/gakki.py
+++ src/gakki.py
@@ -1,4 +1,27 @@
-def gakki(*args):
-    if len(args) == 0:
+import os
+from enum import IntEnum
+
+
+class GakkiType(IntEnum):
+    java = 1
+
+
+class Gakki:
+
+    def __init__(self, project_name, project_type: GakkiType = GakkiType.java):
+        if project_name is None:
+            raise RuntimeError('Project_name is mandatory!')
+        self.project_name = project_name
+        self.project_type = project_type
+
+    def create_project(self):
+        os.makedirs(self.project_name)
+
+
+def gakki(gakki_args: Gakki = None):
+    if gakki_args is None:
         return dict(code=-1, msg='''Usage:
-gakki <java|python|node> [optional libraries]''')
+gakki project_name <java|python|node> [optional libraries]''')
+
+    gakki_args.create_project()
+    return dict(code=0, msg='Created %s project[%s] successfully.' % (gakki_args.project_type, gakki_args.project_name))
--- test/gakki_test.py
+++ test/gakki_test.py
@@ -1,15 +1,30 @@
+import os
 import unittest
 
+from src.gakki import Gakki
 from src.gakki import gakki
+
+PROJECT_NAME = 'for_test'
 
 
 class GakkiTest(unittest.TestCase):
+
+    def tearDown(self):
+        print('Tearing down!')
+        if os.path.exists(PROJECT_NAME):
+            os.rmdir(PROJECT_NAME)
+            print('Removed temporary folder:%s' % PROJECT_NAME)
 
     def test_can_display_usage_when_no_arguments_inputted(self):
         result = gakki()
         self.assertNotEqual(0, result['code'])
         self.assertEqual('''Usage:
-gakki <java|python|node> [optional libraries]''', result['msg'])
+gakki project_name <java|python|node> [optional libraries]''', result['msg'])
+        print(result)
+
+    def test_can_create_java_with_gradle_as_default_project(self):
+        result = gakki(Gakki(PROJECT_NAME))
+        self.assertEqual(0, result['code'])
         print(result)
 
 
